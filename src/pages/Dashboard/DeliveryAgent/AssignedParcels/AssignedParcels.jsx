94dc2442c6b879a6ef4a45312ae30154d1a21555
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-08-03 15:21:25 +0600

    update parcel status functionality implemented

--- src/pages/Dashboard/DeliveryAgent/AssignedParcels/AssignedParcels.jsx
+++ src/pages/Dashboard/DeliveryAgent/AssignedParcels/AssignedParcels.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import useAxiosSecure from "../../../../hooks/useAxios";
 import useAuth from "../../../../hooks/useAuth";
 import Loading from "../../../../components/Shared/Loading/Loading";
+import Swal from "sweetalert2";
 
 
 const AssignedParcels = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
   const agentEmail = user?.email;
+  const queryClient = useQueryClient();
 
   const { data: parcels = [], isLoading, isError, error } = useQuery({
     queryKey: ["assignedParcels", agentEmail],
@@ -19,13 +21,29 @@ const AssignedParcels = () => {
     enabled: !!agentEmail,
   });
 
-  if (isLoading) return <Loading />;
+  const { mutateAsync: updateParcelStatus, isPending } = useMutation({
+    mutationFn: async ({ parcelId, newStatus }) => {
+        return await axiosSecure.patch(`/update-parcel-status/${parcelId}`, {
+        newStatus,
+        });
+    },
+    onSuccess: () => {
+        queryClient.invalidateQueries(["assigned-parcels"]);
+        Swal.fire("Updated", "Parcel status updated successfully", "success");
+    },
+    onError: () => {
+        Swal.fire("Error", "Failed to update status", "error");
+    },
+    });
+
+
+  if (isLoading || isPending) return <Loading />;
   if (isError) return <p className="text-center text-red-600">Error loading parcels: {error.message}</p>;
 
   return (
     <div className="my-10">
         <h2 className="mb-4 font-medium text-xl">All Assigned Parcels</h2>
-        <div className="overflow-x-auto p-4 shadow-xl rounded-2xl">
+        <div className="overflow-x-auto py-4 border-2 border-primary shadow-xl rounded-2xl">
         <table className="table w-full">
         <thead>
           <tr className="text-center">
@@ -62,7 +80,26 @@ const AssignedParcels = () => {
                 </td>
                 <td>{parcel.paymentStatus || "Pending"}</td>
                 <td>{parcel.deliveryStatus}</td>
-                <td><button className="btn btn-primary">Update</button></td>
+                <td>
+                    <select
+                        className="select select-sm select-bordered"
+                        defaultValue=""
+                        onChange={(e) => {
+                            const newStatus = e.target.value;
+                            if (newStatus) {
+                            updateParcelStatus({ parcelId: parcel._id, newStatus });
+                            }
+                        }}
+                        >
+                        <option value="" disabled>
+                            Select Status
+                        </option>
+                        <option value="Picked Up">Picked Up</option>
+                        <option value="In Transit">In Transit</option>
+                        <option value="Delivered">Delivered</option>
+                        <option value="Failed">Failed</option>
+                        </select>
+                </td>
               </tr>
             ))
           )}
